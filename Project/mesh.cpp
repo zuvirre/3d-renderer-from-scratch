e72b54869f497d5be0ce86823a14239dbc7fc451
oamusatov@edu.hse.ru <oamusatov@edu.hse.ru>
2025-03-18 15:10:17 +0300

    Done mesh class (at least for now).

--- Project/mesh.cpp
+++ Project/mesh.cpp
@@ -1,4 +1,5 @@
 #include "mesh.h"
+
 namespace Renderer {
 Mesh::Mesh(ColorFunction ambient, ColorFunction diffuse, ColorFunction specular)
         : ambient_color_function_(ambient),
@@ -7,7 +8,46 @@ Mesh::Mesh(ColorFunction ambient, ColorFunction diffuse, ColorFunction specular)
 }
 
 void Mesh::SetNormalFunction(NormalFunction normal_function) {
-    normal_function_ = normal_function;
+  normal_function_ = normal_function;
+}
+
+Iterable<Mesh::VertsConstIterator> Mesh::GetVerts() const {
+  return Iterable(vertices_.begin(),vertices_.end());
+}
+
+Iterable<Mesh::PolygonConstIterator> Mesh::GetPolygons() const {
+  return Iterable(polygons_.begin(),polygons_.end());
+}
+
+Triangle Mesh::MakeTriangleFromPolygon(const Polygon &polygon) const {
+  Triangle result;
+  for (size_t i = 0; i < 3; ++i) {
+    result.GetVerts()[i] = vertices_[polygon.points_(i)];
+  }
+  result.SetColorFunction(&ambient_color_function_, &diffuse_color_function_,
+                          &specular_color_function_);
+  if (normal_function_) {
+    result.SetNormalFunction(&normal_function_);
+  }
+  return result;
+}
+
+void Mesh::AddPolygon(const Mat3D &coords) {
+  Vector3D first_vec = coords.row(1) - coords.row(0);
+  Vector3D second_vec = coords.row(2) - coords.row(0);
+  Vector3D normal = first_vec.cross(second_vec);
+  normal.normalize();
+  int curr = vertices_.size();
+  for (int i = 0; i < 3; ++i) {
+    Vertex vert{.coordinates = static_cast<Vector3D>(coords.row(i).eval()),
+                .normal = normal};
+    vertices_.push_back(vert);
+  }
+  polygons_.push_back(Polygon{Vector3I{curr, curr + 1, curr + 2}});
+}
+
+void Mesh::AddPolygon(const Mat3D & coords, const Mat3D &normals) {
+  
 }
 
 }  // namespace Renderer
--- Project/mesh.h
+++ Project/mesh.h
@@ -14,6 +14,7 @@ public:
         Vector3I points_;
     };
     using Vector3D = Eigen::Vector3d;
+    using Mat3D = Eigen::Matrix3d;
     using ColorFunction = std::function<Color(const Triangle &, const Vector3D &)>;
     using NormalFunction = std::function<Vector3D(const Triangle &, const Vector3D &)>;
     using VertsConstIterator = std::vector<Vertex>::const_iterator;
@@ -26,9 +27,11 @@ public:
     Iterable<PolygonConstIterator> GetPolygons() const;
 
     Triangle MakeTriangleFromPolygon(const Polygon &) const;
+    void AddTriangle(const Mat3D &);
+    void AddTriangle(const Mat3D &, const Mat3D &);
 private:
     std::vector<Vertex> vertices_;
-    std::vector<Polygon> triangles_;
+    std::vector<Polygon> polygons_;
     ColorFunction ambient_color_function_;
     ColorFunction diffuse_color_function_;
     ColorFunction specular_color_function_;
